950336b69c52260c5a07ad2dd394bc48004da252
Ryan James Spencer <spencer.ryanjames@gmail.com>
2022-06-04 12:35:07 +1000

    Outfit builder

--- qc-core/src/lib.rs
+++ qc-core/src/lib.rs
@@ -18,13 +18,22 @@ pub fn shrink_u64(number: u64) -> Option<u64> {
     Some(number / 2)
 }
 
-pub fn gen_vec_i64(size: usize) -> Vec<i64> {
+pub fn gen_vec<A>(size: usize, gen_element: fn(usize) -> A) -> Vec<A> {
+    let length = rand::random::<usize>() % size;
+    (0..length).into_iter().map(|_| gen_element(size)).collect()
+}
+
+pub fn gen_vec_even_i64(size: usize) -> Vec<i64> {
     let length = rand::random::<usize>() % size;
     (0..length)
         .into_iter()
         .map(|_| gen_i64(size))
         .filter(|x| x % 2 == 0)
         .collect()
+}
+
+pub fn gen_vec_i64(size: usize) -> Vec<i64> {
+    gen_vec(size, gen_i64)
 }
 
 // basically next on an Iterator.
@@ -34,7 +43,6 @@ pub fn shrink_vec_i64(vec: Vec<i64>) -> Option<Vec<i64>> {
     if vec.is_empty() {
         None
     } else {
-        // TODO: for now, shrink the len, but later, also shrink the elements.
         Some(
             vec.into_iter()
                 .take(len - 1)
@@ -69,6 +77,17 @@ pub fn collatz(number: u64) -> u64 {
 
 pub fn prop_collatz_always_one(input: &u64) -> bool {
     collatz(*input) == 1
+}
+
+pub fn abs(number: i64) -> i64 {
+    if number < 0 {
+        return -number;
+    }
+    number
+}
+
+pub fn prop_abs_always_positive(input: &i64) -> bool {
+    abs(*input) >= 0
 }
 
 pub fn report<A: Debug>(rounds: usize, witness: A) {
@@ -129,6 +148,58 @@ pub fn qc<A: Debug + Clone>(
     }
 }
 
+pub struct Qc<A> {
+    runs: usize,
+    size: usize, // TODO: ought to be optional.
+    gen: Option<fn(usize) -> A>,
+    shrink: Option<fn(A) -> Option<A>>,
+}
+
+impl<A: Debug + Clone> Qc<A> {
+    pub fn new() -> Self {
+        Qc {
+            runs: 100,
+            size: 100,
+            gen: None,
+            shrink: None,
+        }
+    }
+
+    pub fn with_gen(self, gen: fn(usize) -> A) -> Self {
+        Qc {
+            gen: Some(gen),
+            ..self
+        }
+    }
+
+    pub fn with_shrink(self, shrink: fn(A) -> Option<A>) -> Self {
+        Qc {
+            shrink: Some(shrink),
+            ..self
+        }
+    }
+
+    pub fn with_runs(self, runs: usize) -> Self {
+        Qc { runs, ..self }
+    }
+
+    pub fn with_size(self, size: usize) -> Self {
+        Qc { size, ..self }
+    }
+
+    pub fn check(self, property: fn(&A) -> bool) {
+        assert!(self.gen.is_some());
+        assert!(self.shrink.is_some());
+        qc(
+            property,
+            self.gen.unwrap(),
+            self.shrink.unwrap(),
+            self.size,
+            self.runs,
+        )
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -142,23 +213,9 @@ mod test {
     //    assert!(false);
     //}
 
-    //#[test]
-    //fn prop_all_even_works() {
-    //    assert!(prop_all_even(&Vec::new()));
-    //    assert!(prop_all_even(&[2].into_iter().collect()));
-    //    assert!(prop_all_even(&[4, 8].into_iter().collect()));
-    //}
-
-    //#[test]
-    //fn prop_collatz_always_one_works() {
-    //    for n in 0..100 {
-    //        assert!(prop_collatz_always_one(&n));
-    //    }
-    //}
-
     #[test]
     fn playground() {
-        qc(prop_all_even, gen_vec_i64, shrink_vec_i64, 100, 100);
+        qc(prop_all_even, gen_vec_even_i64, shrink_vec_i64, 100, 100);
         qc(
             prop_collatz_always_one,
             gen_u64,
@@ -170,5 +227,23 @@ mod test {
             //u64::MAX as usize,
             100,
         );
+        qc(prop_abs_always_positive, gen_i64, shrink_i64, 100, 100);
+
+        Qc::new()
+            .with_gen(gen_vec_even_i64)
+            .with_shrink(shrink_vec_i64)
+            .check(prop_all_even);
+
+        Qc::new()
+            .with_gen(gen_u64)
+            .with_shrink(shrink_u64)
+            .with_size(1000)
+            .check(prop_collatz_always_one);
+
+        Qc::new()
+            .with_gen(gen_i64)
+            .with_shrink(shrink_i64)
+            .with_size(i64::MAX as usize)
+            .check(prop_abs_always_positive);
     }
 }
